a9aa048cad020203278d84642e0b8b717e1df338
Santiago Jaramillo <66808131+siosantiago@users.noreply.github.com>
2022-02-14 23:25:20 -0800

    work now

--- PasswordChecker.c
+++ PasswordChecker.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+#include "passwords.c"
+
+int main(int argc, char *argv[]) {
+
+  char *str = argv[1];
+  int found = 0;
+
+  
+  int i = 0;
+  while(i < (int)( sizeof(passwords) / sizeof(passwords[0]) ) && found == 0) {
+  int ctr = 0;
+  char *current = passwords[i];
+  
+    while(current[ctr] == str[ctr]) {
+      if(current[ctr]=='\0'|| str[ctr]=='\0') {
+            break;
+      }
+      ctr++;
+  }
+    if(current[ctr]=='\0' && str[ctr]=='\0')
+        found = 1;
+  i++;
+}
+
+  if (found == 1) {
+    printf("is present in this array.\n");
+  } else{
+    printf("is present in this array.\n");
+
+  }
+}
